40aa7ea4c3d6ee7dd1a1d5fd6d3d58e9802a4c53
Connor Przybyla <connor.przybyla@superiortech.io>
2018-06-25 11:57:30 -0400

    Added tests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tests
+This repository uses the Web3 API in Javascript to:
+- Connect to a Parity node
+- Connect to a smart contract on the Ethereum blockchain
+- Call functions within a Solidity smart contract
+
+Using Mozilla performance.now(), speed tests are performed to
+calculate the response time for making a request to get data
+off of the Ethereum blockchain. For my testing purposes, I connected
+to a MetaMask wallet and connected to a smart contract on the Ropsten TestNet.
---
+++ contract.js
@@ -0,0 +1,37 @@
+
+    var Web3 = require('web3');
+    var now = require("performance-now");
+
+    // Connect to Ethereum smart contract through Parity
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")); // Parity default
+    }
+
+    // Read contract abi, and connect to its address
+    // this allows us to use the web3 api to call
+    // functions within a smart contract
+    var fs = require('fs')
+    var abiArray = JSON.parse(fs.readFileSync('abi.json', 'utf-8'));
+    var contractAddress = "CONTRACT ADDRESS HERE";
+    var contractInstance = new web3.eth.Contract(abiArray, contractAddress);
+
+
+    // Test #1 - Return the balance of a wallet address
+    var t0 = now();
+    var result = contractInstance.methods.balanceOf('WALLET ADDRESS HERE').call();
+    var t1 = now();
+    console.log("Call to balanceOf took " + (t1 - t0) + " milliseconds.");
+
+    // Test #2 - Return the total supply of the token
+    t0 = now();
+    result = contractInstance.methods.totalSupply().call();
+    t1 = now();
+    console.log("Call to totalSupply took " + (t1 - t0) + " milliseconds.");
+
+    // Test #3 - Return the amount of tokens the spender is allowed to use from the owner's adress
+    t0 = now();
+    result = contractInstance.methods.allowance('TOKEN OWNER ADDRESS HERE','SPENDER ADDRESS HERE').call();
+    t1 = now();
+    console.log("Call to allowance took " + (t1 - t0) + " milliseconds.");
